380cfb0c8a050a9deab4c34b769eab85e38ff1bd
gianlucacosta-eng <gianluca.costa@aluno.edu.es.gov.br>
2026-07-01 08:02:22 -0300

    Create index.php

---
+++ aula02-php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta chaset="UTF-8">
+    <title>Aula 02</title>
+</head>
+<body>
+    <?php
+    $nome = "Jainy";
+    $idade = "16";
+    $nome2 = "Gianluca";
+    $idade2 = "16";
+
+
+    echo "Meu nome é "  .$nome ."e minha idade  é " .$idade ."o nome da minha dupla é " .$nome2 ."e a idade dele  é " .$idade2;
+    ?>
+</body>
+</html>
